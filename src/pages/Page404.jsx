385145b43e5451138dd479465a9142a3b3bf6303
saadbenallal4 <saadbenallal4@gmail.com>
2026-02-05 14:25:55 +0100

    Création de la page d'erreur 404 avec style

--- src/pages/Page404.jsx
+++ src/pages/Page404.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Page404.scss";
 
 function Page404() {
     return (
-        <div>
-            <h1>404 - Page non trouvée</h1>
-            <p>Oups! La page que vous demandez n'existe pas.</p>
+        <div className="page-404">
+            <h1 className="error-code">404</h1>
+            <p className="error-text">Oups! La page que vous demandez n'existe pas.</p>
+            <Link to="/" className="back-home">Retourner sur la page d’accueil</Link>
         </div>
     );
 }
